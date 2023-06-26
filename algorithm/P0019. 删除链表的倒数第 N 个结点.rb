c305aba16756cf39992c92cb2e9fdb051b0dbf9f
spriteboysz <sprite_bx@126.com>
2023-06-26 22:58:02 +0800

    2023062622

--- algorithm/P0019. 删除链表的倒数第 N 个结点.rb
+++ algorithm/P0019. 删除链表的倒数第 N 个结点.rb
@@ -11,8 +11,22 @@ load "common/leetcode.rb"
 # @param {ListNode} head
 # @param {Integer} n
 # @return {ListNode}
-def remove_nth_from_end(head, n) end
+def remove_nth_from_end(head, n)
+    nodes = []
+    until head.nil?
+        nodes << head
+        head = head.next
+    end
+    return nil if n > nodes.length || nodes.length == 1
+    nodes.delete_at(-n)
+    nodes.each_with_index { |node, i|
+        next if i == nodes.length - 1
+        node.next = nodes[i + 1]
+    }
+    nodes.last.next = nil
+    nodes[0]
+end
 
 if __FILE__ == $PROGRAM_NAME
-
+    p remove_nth_from_end(ListNode.create("[1,2,3,4,5]"), 2).to_s
 end
