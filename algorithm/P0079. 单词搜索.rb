ec51cf301ac2b694c5329a6195b4d34aaca67ded
spriteboysz <sprite_bx@126.com>
2023-08-19 10:55:07 +0800

    2023081910

--- algorithm/P0079. 单词搜索.rb
+++ algorithm/P0079. 单词搜索.rb
@@ -11,25 +11,23 @@ load "common/leetcode.rb"
 # @param {Character[][]} board
 # @param {String} word
 # @return {Boolean}
+@directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
 def exist(board, word)
     @board, @word = board, word
 
-    def dfs(i, j, index)
-        p [index, @board[i][j], @word[index], @word.length]
-        return false if @board[i][j] != @word[index]
-        p [index, @word.length]
-        # return true if index == @word.length + 100
-
-        temp, @board[i][j] = @board[i][j], "#"
-        [[0, 1], [0, -1], [1, 0], [-1, 0]].each do |dx, dy|
-            x, y = dx + i, dy + j
-            if x >= 0 && x < @board.length && y >= 0 && y < @board[0].length && dfs(x, y, index + 1)
-                # return true
-            end
+    def dfs(i, j, k)
+        if i < 0 || j < 0 || i >= @board.length || j >= @board[0].length || @board[i][j] != @word[k]
+            return false
         end
-        @board[i][j] = temp
+        return true if k == @word.length - 1
+        @board[i][j] = ""
+        res = dfs(i + 1, j, k + 1) || dfs(i - 1, j, k + 1) || dfs(i, j + 1, k + 1) || dfs(i, j - 1, k + 1)
+        @board[i][j] = @word[k]
+        res
     end
 
+    return false if board.flatten.uniq.length < word.split('').uniq.length
     board.each_with_index do |row, i|
         row.each_index do |j|
             return true if dfs(i, j, 0)
@@ -39,7 +37,6 @@ def exist(board, word)
 end
 
 if __FILE__ == $PROGRAM_NAME
-    p exist([%w[A B C E], %w[S F C S], %w[A D E E]], "ABCCED")
     p exist([%w[A A A A A A],
              %w[A A A A A A],
              %w[A A A A A A],
